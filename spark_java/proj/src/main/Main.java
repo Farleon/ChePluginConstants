04156bb7dbca91e4996bf1e736b041e9ca22ba2f
Farleon <karel.vandenhoute@gmail.com>
2018-04-19 16:56:58 +0200

    Update Main.java

--- spark_java/proj/src/main/Main.java
+++ spark_java/proj/src/main/Main.java
@@ -2,7 +2,7 @@
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
  
-public class SparkSample {
+public class Main {
  
 	public static void main(String[] args) {
  
